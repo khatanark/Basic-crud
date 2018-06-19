418ba500c62b5ccf6338985222e90a0a842d9cb1
khatanark <rohitkhatana16@gmail.com>
2018-06-19 14:59:32 +0530

    show post- find method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,4 +2,10 @@ class PostsController < ApplicationController
 	def index
 		@posts= Post.all
 	end 
+   
+   # instace variables are accesible inside the views. 
+   # params grabs the id from the url.
+	def show 
+		@post= Post.find(params[:id])
+	end 
 end
