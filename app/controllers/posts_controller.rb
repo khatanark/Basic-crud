db03560d4464a53609db5fb22555b2748ebf82eb
khatanark <rohitkhatana16@gmail.com>
2018-06-19 22:13:47 +0530

    create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,34 @@ class PostsController < ApplicationController
 	def show 
 		@post= Post.find(params[:id])
 	end 
+
+	def new
+		# empty post object is required 
+		# We have the access to this empty object in view.
+		@post = Post.new
+	end 
+       
+       #  Form is submitted to this action 
+	def create 
+       @post = Post.new(post_params)
+       if @post.save
+       	 redirect_to posts_path , :notice => "Your post was saved"
+       	else
+       		render "new"
+       end
+	end
+
 end
+
+
+# def create
+#   @article = Article.new(article_params)
+ 
+#   @article.save
+#   redirect_to @article
+# end
+ 
+private
+  def post_params
+    params.require(:post).permit(:title, :content)
+  end
